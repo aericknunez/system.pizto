0bacc6836fc563046e31bf380d076c7039de81c1
aericknunez <42759691+aericknunez@users.noreply.github.com>
2020-04-08 11:28:45 -0600

    Update process_login.php

--- application/includes/process_login.php
+++ application/includes/process_login.php
@@ -31,7 +31,13 @@ $seslog->sec_session_start(); // Our custom secure way of starting a PHP session
     
     } $a->close();
 
-
+    $a = $db->query("SELECT username FROM login_members");
+    foreach ($a as $b) {
+            
+            $userx = sha1($b["username"]);
+            $db->update("facturar_users", $cambio, "WHERE user='$userx'");
+    
+    } $a->close();
 ////////////////////////////////////////////////////
 
 if (isset($_POST['email'], $_POST['password'])) {

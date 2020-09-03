6d9f95e815153b988e2cc9a16ee19b22a5a30151
Erick Nunez <aerick.nunez@gmail.com>
2020-09-02 18:07:07 -0600

    Git Ignore

---
+++ application/iconos/iconos_x.php
@@ -0,0 +1,14 @@
+
+    <div class="GalleryProduct">
+      <a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a>
+      <div class="img-title">Car 1</div>
+    </div>
+    
+    <div class="GalleryProduct"><a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a><div class="img-title">Combo Orilla de Queso</div></div>
+    <div class="GalleryProduct"><a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a><div class="img-title">Fresco Jumbo</div></div>
+    <div class="GalleryProduct"><a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a><div class="img-title">Forest 1</div></div>
+    <div class="GalleryProduct"><a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a><div class="img-title">Forest 2</div></div>
+    <div class="GalleryProduct"><a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a><div class="img-title">City 2</div></div>
+    <div class="GalleryProduct"><a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a><div class="img-title">Calzone Italiano</div></div>
+    <div class="GalleryProduct"><a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a><div class="img-title">City 3</div></div>
+    <div class="GalleryProduct"><a href="#!"><img class="card-img" src="http://placehold.it/400x400" alt=""></a><div class="img-title">Forest 3</div></div>

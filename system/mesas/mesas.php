0ff1084596e716ee2b1e931e5e22e3531e2f3278
Erick Nunez <aerick.nunez@gmail.com>
2020-11-26 11:06:37 -0600

    Update mesas.php

--- system/mesas/mesas.php
+++ system/mesas/mesas.php
@@ -26,6 +26,7 @@ $a = $db->query("SELECT * FROM mesa WHERE estado = 1 and tipo = 2 and tx = ".$_S
 	<figure class="figure">
 	    <img src="assets/img/imagenes/'.Helpers::Mesa($b["clientes"]).'" class="figure-img img-fluid z-depth-2 rounded-circle"  alt="hoverable" >
 	    <figcaption class="figure-caption text-center">'.$mesan.'</figcaption>
+      <figcaption class="figure-caption text-center">'.$b["empleado"].'</figcaption>
 	</figure>
 	</a>  &nbsp &nbsp &nbsp';
 	unset($nmesa);

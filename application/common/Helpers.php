c4a1afffba36e71c63604d4e0b38a4590f5da72a
Erick Nunez <aerick.nunez@gmail.com>
2021-03-10 19:47:31 -0600

    Update Helpers.php

--- application/common/Helpers.php
+++ application/common/Helpers.php
@@ -10,6 +10,7 @@ class Helpers{
     public static function ServerDomain(){
           if($_SERVER["SERVER_NAME"] == "pizto.com" 
           or $_SERVER["SERVER_NAME"] == "www.pizto.com"
+          or $_SERVER["SERVER_NAME"] == "system.hibridosv.com"
           or $_SERVER["SERVER_NAME"] == "superpollo.net"
           or $_SERVER["SERVER_NAME"] == "www.superpollo.net"){
             return TRUE;

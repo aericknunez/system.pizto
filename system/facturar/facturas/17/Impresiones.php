296b62b9e3cdd43bba0d7a31b067c25c89a62276
aericknunez <aerick.nunez@gmail.com>
2021-03-28 20:10:09 -0600

    Update Impresiones.php

--- system/facturar/facturas/17/Impresiones.php
+++ system/facturar/facturas/17/Impresiones.php
@@ -13,7 +13,7 @@ class Impresiones{
  
  public function Ticket($efectivo, $numero){
   $db = new dbConn();
-  $nombre_impresora = "LR2000";
+  $nombre_impresora = "PRECUENTA";
   $img  = "C:/AppServ/www/pizto/assets/img/logo_factura/grosera.jpg";
 
 
@@ -189,7 +189,7 @@ $printer->close();
  public function ImprimirAntes($efectivo, $numero, $cancelar){
   $db = new dbConn();
 
-  $nombre_impresora = "LR2000";
+  $nombre_impresora = "PRECUENTA";
   $img  = "C:/AppServ/www/pizto/assets/img/logo_factura/grosera.jpg";
 
 
@@ -417,7 +417,7 @@ $a = $db->query("select ticket_temp.cod as cod, ticket_temp.hash as hash, ticket
 
 
 
-$nombre_impresora = "LR2000";
+$nombre_impresora = "PRECUENTA";
 
 $connector = new WindowsPrintConnector($nombre_impresora);
 $printer = new Printer($connector);
@@ -733,7 +733,7 @@ $printer->close();
 
 
  public function AbrirCaja(){
-$nombre_impresora = "LR2000";
+$nombre_impresora = "PRECUENTA";
 
 $connector = new WindowsPrintConnector($nombre_impresora);
 $printer = new Printer($connector);
@@ -754,7 +754,7 @@ $printer->close();
 
 
 
-  $nombre_impresora = "LR2000";
+  $nombre_impresora = "PRECUENTA";
 
 
 $connector = new WindowsPrintConnector($nombre_impresora);
@@ -1044,7 +1044,7 @@ $a = $db->query("select ticket_borrado.cod as cod, ticket_borrado.hash as hash, 
 
  if($cantidadproductos > 0){
 
-$nombre_impresora = "LR2000";
+$nombre_impresora = "PRECUENTA";
 
 $connector = new WindowsPrintConnector($nombre_impresora);
 $printer = new Printer($connector);

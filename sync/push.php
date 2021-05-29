6ed10a5b85e3e72d42316b5bcfc97054ea27f91b
aericknunez <aerick.nunez@gmail.com>
2021-05-29 12:15:46 -0600

    Update push.php

--- sync/push.php
+++ sync/push.php
@@ -47,14 +47,15 @@ $sync = $pushed->Execute($_REQUEST["corte"]);
 if($sync != NULL){
 	if(SubirFtp($sync) == TRUE){
 		@unlink($sync . ".sql");
-
-		$data =  file_get_contents('https://data.hibridosv.com/sync/import_i.php?x=' . $_SESSION["temporal_td"]);
-		$datos = json_decode($data, true);
 	}
 } 
 
-unset($_SESSION["temporal_td"]);
 
+		$data =  file_get_contents('https://data.hibridosv.com/sync/import_i.php?x=' . $_SESSION["temporal_td"]);
+		$datos = json_decode($data, true);
+
+unset($_SESSION["temporal_td"]);
+unset($datos);
 
 
 
